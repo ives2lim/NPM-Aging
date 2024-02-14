db707f24fae8f73ee9ec73bf1da324ea612aa009
ives2lim <77058994+ives2lim@users.noreply.github.com>
2024-02-14 16:42:34 +0800

    Create remove_outliers.R

---
+++ Rcmds/remove_outliers.R
@@ -0,0 +1,17 @@
+remove_outliers <- function(variable, IQRcutoff=2){
+
+        temp <- variable
+        temp.median <- quantile(temp , 0.5, na.rm=TRUE)
+        temp.IQR <- quantile(temp , 0.75, na.rm=TRUE) - quantile(temp , 0.25, na.rm=TRUE)
+        upperoutlier <- temp.median + abs(IQRcutoff)*temp.IQR
+        loweroutlier <- temp.median - abs(IQRcutoff)*temp.IQR
+        changeupper <- I(temp > upperoutlier)
+        changelower <- I(temp < loweroutlier)
+        #max.nonoutlier <- max(temp[changelower==0&changeupper==0], na.rm=TRUE)
+        #min.nonoutlier <- min(temp[changelower==0&changeupper==0], na.rm=TRUE)
+        temp[changeupper] <- NA
+        temp[changelower] <- NA
+        rm(temp.median, temp.IQR, upperoutlier, loweroutlier, changeupper, changelower)
+        return(temp)
+
+  }
